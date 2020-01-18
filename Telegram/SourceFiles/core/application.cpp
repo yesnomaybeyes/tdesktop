yesnomaybeyes/tdesktop

commit c2ff84030701d05b677e209d2865dd1be8642a66

Added new setting to use original tray icon.
 - Returned the original tray icons.
 - Refactored work with custom tray icons.
 - Moved Forkgram icons to the "art/forkgram" folder.
 - Removed unused files.
 - Fixed #26.

Telegram/SourceFiles/core/application.cpp
@@ -205,14 +205,6 @@ void Application::run() {
 	// Create mime database, so it won't be slow later.
 	QMimeDatabase().mimeTypeForName(qsl("text/plain"));
 
-	Local::ReadMapState state = Local::readMap(QByteArray());
-
-	style::SetSquareUserpics(Global::SquareAvatars());
-	// Moved readMap to place before creating main window to change tray icon.
-	if (Global::SquareAvatars()) {
-		_logoNoMargin = _logo = Window::LoadLogoSquare();
-	}
-
 	_window = std::make_unique<Window::Controller>(&activeAccount());
 
 	const auto currentGeometry = _window->widget()->geometry();
@@ -231,6 +223,7 @@ void Application::run() {
 	startShortcuts();
 	App::initMedia();
 
+	Local::ReadMapState state = Local::readMap(QByteArray());
 	if (state == Local::ReadMapPassNeeded) {
 		Global::SetLocalPasscode(true);
 		Global::RefLocalPasscodeChanged().notify();

Telegram/Resources/langs/lang.strings
@@ -302,6 +302,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 "lng_settings_search_engine_field_label" = "URL must contain '%q'";
 "lng_settings_show_all_recent_stickers" = "Show all Recently used Stickers";
 "lng_settings_use_black_tray_icon" = "Use black tray icon";
+"lng_settings_use_original_tray_icon" = "Use original tray icon";
 
 "lng_settings_custom_sticker_size" = "Custom Sticker Size";
 "lng_settings_sticker_size_label" = "Allowed values are between 50 and 256.";

Telegram/Resources/qrc/telegram/telegram.qrc
@@ -47,6 +47,8 @@
     <file alias="art/logo_256.png">../../art/logo_256.png</file>
     <file alias="art/logo_256_square.png">../../art/logo_256_square.png</file>
     <file alias="art/logo_256_no_margin.png">../../art/logo_256_no_margin.png</file>
+    <file alias="art/forkgram/logo_256.png">../../art/forkgram/logo_256.png</file>
+    <file alias="art/forkgram/logo_256_no_margin.png">../../art/forkgram/logo_256_no_margin.png</file>
     <file alias="art/sunrise.jpg">../../art/sunrise.jpg</file>
     <file alias="day-blue.tdesktop-theme">../../day-blue.tdesktop-theme</file>
     <file alias="night.tdesktop-theme">../../night.tdesktop-theme</file>

commit 85e2b7d637d2daa6c03543d8c8466ad3288e4520

Fixed round unread budges.
Regression was introduced in c2ff840307.

Telegram/SourceFiles/core/application.cpp
@@ -224,6 +224,7 @@ void Application::run() {
 	App::initMedia();
 
 	Local::ReadMapState state = Local::readMap(QByteArray());
+	style::SetSquareUserpics(Global::SquareAvatars());
 	if (state == Local::ReadMapPassNeeded) {
 		Global::SetLocalPasscode(true);
 		Global::RefLocalPasscodeChanged().notify();